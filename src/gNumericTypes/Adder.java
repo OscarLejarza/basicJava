f5e41be4d9669529cd52e0c4d809324e65d9183b
Oscar Lejarza <oscar.lejarza@astrazeneca.com>
2020-06-08 20:26:03 -0500

    agregando tareas

--- src/gNumericTypes/Adder.java
+++ src/gNumericTypes/Adder.java
@@ -7,14 +7,14 @@ public class Adder {
 	public static void main(String args[])
 	{
 		// prompt user for x
-		System.out.print("x is: "); // funcion para imprimir en consola
-	    int x = Input.get_int(); //
+		System.out.print("x is: "); // funcion para imprimir el texto en consola
+	    int x = Input.get_int(); // pide al usuario el valor de x
 	    
 	    // prompt user for y
-	    System.out.print("y is: ");
-	    int y = Input.get_int();
+	    System.out.print("y is: ");// funcion para imprimir el texto en consola
+	    int y = Input.get_int(); // pide al usuario el valor de y
 	    
 	    // calculate sum for user
-	    System.out.println("sum is " + (x + y) + "\n");   
+	    System.out.println("sum is " + (x + y) + "\n"); // imprime el texto, con la suma de las variables y un salto de linea
 	}
 }
