6b356282a5cef2c1bb15c260c673347cc088613f
Oscar Lejarza <oscar.lejarza@astrazeneca.com>
2020-06-12 08:07:37 -0500

    agregando tarea

---
+++ src/jStrings/InicialesFacil.java
@@ -0,0 +1,20 @@
+package jStrings;
+
+import libs.Input;
+
+
+public class InicialesFacil {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Ingresa tu nombre"); // imprime texto
+        String nombre = Input.get_string(); // pide un input de tipo String
+        String[] lista = nombre.split(" "); // separa la cadena de texto por los espacios
+        for(int i = 0; i < lista.length; i++){ // ciclo para recorrer la longitus de la lista
+            Input.print(lista[i].charAt(0)); // imprime de la lista de caracteres solo los chars en la primera posicion
+        }
+        System.out.println("\n");
+
+    }
+}
